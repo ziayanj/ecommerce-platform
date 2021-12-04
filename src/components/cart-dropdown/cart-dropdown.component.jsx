6f82a54bdf7fa69f286c6b52f94aa605e6389a88
Ziayan Javed <ziayanj@gmail.com>
2021-12-04 13:00:17 +0500

    Add cart icon and its dropdown

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,13 @@
+import CustomButton from "../custom-button/custom-button.component";
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+    </div>
+    <CustomButton>Go to checkout</CustomButton>
+  </div>
+);
+
+export default CartDropdown;
