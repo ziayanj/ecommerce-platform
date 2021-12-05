1d89dbc4a04fc4f9709316c2e9134fb0173bb66e
Ziayan Javed <ziayanj@gmail.com>
2021-12-05 09:43:21 +0500

    Implement adding to cart functionality

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import { cartActionTypes } from "./cart.types";
 export const toggleCartDropdown = () => ({
   type: cartActionTypes.TOGGLE_CART_DROPDOWN
 });
+
+export const addItem = (item) => ({
+  type: cartActionTypes.ADD_ITEM,
+  payload: item
+});
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -14,6 +14,6 @@ const userReducer = (state=INITIAL_STATE, action) => {
     default:
       return state;
   }
-}
+};
 
 export default userReducer;
