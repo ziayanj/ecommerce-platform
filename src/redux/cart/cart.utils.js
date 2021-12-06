380cfd3c0f6ef355462291b0ca3fc6ace2fe5ef4
Ziayan Javed <ziayanj@gmail.com>
2021-12-07 01:00:32 +0500

    Implement add/remove options inside checkout

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,14 +4,28 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   );
 
   if (existingCartItem) {
-    existingCartItem.quantity += 1;
-    return [...cartItems];
+    // existingCartItem.quantity += 1;
+    // return [...cartItems];
     
-    // return cartItems.map(cartItem => 
-    //   cartItem.id === cartItemToAdd.id
-    //     ? { ...cartItem, quantity: cartItem.quantity + 1 }
-    //     : cartItem
-    // );  
+    return cartItems.map(cartItem => 
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );  
   }
   return [...cartItems, {...cartItemToAdd, quantity: 1}];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem => 
+    cartItem.id === cartItemToRemove.id
+    ? {...cartItem, quantity: cartItem.quantity - 1}
+    : cartItem  
+  );
 };
