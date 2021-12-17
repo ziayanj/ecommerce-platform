b34dea7e4e4b4ad46f3729449cddac6cadaa5359
Ziayan Javed <ziayanj@gmail.com>
2021-12-17 12:20:38 +0500

    Move directory and shop data to Redux store, using selectors inside respective components. Also, use redux-persist for persistance of Cart state

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -9,20 +9,21 @@ const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
   
   return (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
     </div>
-    <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
-  </div>
-)};
+  )
+};
 
 const mapDispatchToProps = (dispatch) => ({
   addItem: (item) => dispatch(addItem(item))
