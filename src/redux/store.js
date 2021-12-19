68825d7789415dcf1541130e2eba1965c3e54160
Ziayan Javed <ziayanj@gmail.com>
2021-12-19 13:48:01 +0500

    Make redux-logger development-only

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger';
 
 import persistedReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 
